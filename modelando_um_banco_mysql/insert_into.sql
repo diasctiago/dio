1d7ebec71e41afefd53fb16f3e5a24c0766a4f1a
Tiago Dias <63130021+diasctiago@users.noreply.github.com>
2022-03-14 13:16:44 -0300

    Update insert_into.sql

--- modelando_um_banco_mysql/insert_into.sql
+++ modelando_um_banco_mysql/insert_into.sql
@@ -1,1 +1,9 @@
-
+# Inserindo dados na tabela movies
+INSERT INTO 
+    movies (id, type, name, genre, total_ep, atual_ep, last_view) 
+    VALUES  
+        (1, 0, 'La Casa de PaPel', 'Ação', 41, 41, '2022-02-01'),
+        (2, 0, 'Prision Break', 'Ação', 90, 90, '2017-12-01'),
+        (3, 1, 'Velozes & Furiosos 9', 'Ação', '', '', '2021-08-10'),
+        (4, 1, 'Palmer', 'Drama', '', '', '2021-03-11'),
+        (5, 1, 'Alerta Vermelho', 'Ação', '', '', '2021-12-01');
