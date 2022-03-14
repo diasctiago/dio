bd3d769e62ed56daee34d65d465de8912741993d
Tiago Dias <63130021+diasctiago@users.noreply.github.com>
2022-03-14 13:16:34 -0300

    Update create_table.sql

--- modelando_um_banco_mysql/create_table.sql
+++ modelando_um_banco_mysql/create_table.sql
@@ -1,1 +1,10 @@
-
+# Criando a tabela movies
+CREATE TABLE movies (
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    type        INT NOT NULL,       
+    name        VARCHAR (50),
+    genre       VARCHAR (20),
+    total_ep    INT,
+    atual_ep    INT,
+    last_view   DATETIME DEFAULT CURRENT_TIMESTAMP
+);
